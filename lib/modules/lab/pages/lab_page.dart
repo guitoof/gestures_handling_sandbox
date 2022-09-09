c97d52d622d3ecaa677ab5927e26f008346aa3ef
Damian Bast <me@damianbast.com>
2022-09-09 13:49:34 +0200

    example for a signature pad which on mobile should absorb all gestures and not allow the parents to scroll while the signing gesture is active

--- lib/modules/lab/pages/lab_page.dart
+++ lib/modules/lab/pages/lab_page.dart
@@ -1,12 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:hand_signature/signature.dart';
 
 class LabPage extends StatelessWidget {
   const LabPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Play around with gestures on this page'),
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 3,
+      child: TabBarView(
+        children: [
+          Container(height: 200, color: Colors.pink),
+          ListView(
+            children: [
+              Column(
+                children: [
+                  Row(
+                    children: const [
+                      Flexible(
+                        child: Text(
+                            'The grey section is the signature pad which should absorb all gestures, on mobile this will look a bit funky at the moment'),
+                      ),
+                    ],
+                  ),
+                  const SignatureSection(),
+                  ...List.generate(
+                    20,
+                    (index) {
+                      return Container(height: 300, width: 300, color: index.isEven ? Colors.red : Colors.blue);
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Container(height: 200, color: Colors.green),
+        ],
+      ),
+    );
+  }
+}
+
+class SignatureSection extends StatefulWidget {
+  const SignatureSection({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<SignatureSection> createState() => _SignatureSectionState();
+}
+
+class _SignatureSectionState extends State<SignatureSection> {
+  final control = HandSignatureControl();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey[200],
+      height: 300,
+      width: 300,
+      child: HandSignature(
+        control: control,
+        type: SignatureDrawType.shape,
+      ),
     );
   }
 }
